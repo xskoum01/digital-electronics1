ac3820369e774de91ae2cd88eb33210d209ebb49
student <student@PC-638.urel.feec.vutbr.cz>
2022-03-22 14:51:59 +0100

    06

--- labs/06-counter/counter/counter.srcs/sources_1/new/top.vhd
+++ labs/06-counter/counter/counter.srcs/sources_1/new/top.vhd
@@ -34,7 +34,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity top is
     Port ( CLK100MHZ : in STD_LOGIC;
            SW : in STD_LOGIC;
-           LED : out STD_LOGIC;
+           LED : out std_logic_vector (16-1 downto 0);
            CA : out STD_LOGIC;
            CB : out STD_LOGIC;
            CC : out STD_LOGIC;
@@ -42,13 +42,68 @@ entity top is
            CE : out STD_LOGIC;
            CF : out STD_LOGIC;
            CG : out STD_LOGIC;
-           AN : out STD_LOGIC;
+           AN : out std_logic_vector (8-1 downto 0);
            BTNC : in STD_LOGIC);
 end top;
 
+------------------------------------------------------------------------
+-- Architecture body for top level
+------------------------------------------------------------------------
 architecture Behavioral of top is
+
+  -- Internal clock enable
+  signal s_en  : std_logic;
+  -- Internal counter
+  signal s_cnt : std_logic_vector(4 - 1 downto 0);
 
 begin
 
+  --------------------------------------------------------------------
+  -- Instance (copy) of clock_enable entity
+  clk_en0 : entity work.clock_enable
+      generic map(
+          g_MAX => 25000000
+      )
+      port map(
+          clk   => CLK100MHZ,
+          reset => BTNC,
+          ce_o  => s_en
+      );
 
-end Behavioral;
+  --------------------------------------------------------------------
+  -- Instance (copy) of cnt_up_down entity
+  bin_cnt0 : entity work.cnt_up_down
+     generic map(
+          --- WRITE YOUR CODE HERE
+           g_CNT_WIDTH => 4
+      )
+      port map(
+          --- WRITE YOUR CODE HERE
+           clk      =>  CLK100MHZ,  
+           reset    =>  BTNC, 
+           en_i     =>  s_en,
+           cnt_up_i =>  SW,
+           cnt_o    =>  s_cnt
+      );
+
+  --------------------------------------------------------------------
+  -- Instance (copy) of hex_7seg entity
+  hex2seg : entity work.hex_7seg
+      port map(
+          hex_i    => s_cnt,
+          seg_o(6) => CA,
+          seg_o(5) => CB,
+          seg_o(4) => CC,
+          seg_o(3) => CD,
+          seg_o(2) => CE,
+          seg_o(1) => CF,
+          seg_o(0) => CG
+      );
+
+  -- Connect one common anode to 3.3V
+  AN <= b"1111_1110";
+
+  -- Display counter values on LEDs
+  LED(3 downto 0) <= s_cnt;
+
+end architecture Behavioral;
